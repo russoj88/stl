56c0065bdb38e342989900c0adbf21dfc3c03737
jeff <russoj88@gmail.com>
2019-02-05 19:54:25 -0800

    Fix typo

--- stl/read.go
+++ stl/read.go
@@ -18,7 +18,7 @@ func Read(r io.Reader) (STL, error) {
 
 	// If indicator is "solid" then it is an ASCII file.  Otherwise binary.
 	if string(indicator) == "solid" {
-		return readAscii(br)
+		return readASCII(br)
 	}
 	return readBinary(br)
 }
