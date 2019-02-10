73d1da9e3dc91da8b6634b15a8edfb5e96849430
jeff <russoj88@gmail.com>
2019-02-10 15:45:31 -0800

    Add benchmark comparing ReadFile vs Read

--- stl/stl_benchmark_test.go
+++ stl/stl_benchmark_test.go
@@ -16,13 +16,33 @@ func BenchmarkRead(b *testing.B) {
 		b.Run(fmt.Sprintf("cl=%02d", testLevel), func(b *testing.B) {
 			SetConcurrencyLevel(testLevel)
 			for i := 0; i < b.N; i++ {
-				runForFile(testFile, b)
+				runRead(testFile, b)
 			}
 		})
 	}
 }
-
-func runForFile(testFile string, b *testing.B) {
+func BenchmarkReadFile(b *testing.B) {
+	testFile := "testdata/Utah_teapot.stl"
+	for _, testLevel := range []uint32{
+		// Add different levels of concurrency here to see the best performance on a particular machine
+		// The number of cores (x2 for hyper-threading) seem to get the best performance
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 32, 40, 48, 56, 64,
+	} {
+		b.Run(fmt.Sprintf("cl=%02d", testLevel), func(b *testing.B) {
+			SetConcurrencyLevel(testLevel)
+			for i := 0; i < b.N; i++ {
+				runRead(testFile, b)
+			}
+		})
+		b.Run(fmt.Sprintf("cl=%02d_file", testLevel), func(b *testing.B) {
+			SetConcurrencyLevel(testLevel)
+			for i := 0; i < b.N; i++ {
+				runReadFile(testFile, b)
+			}
+		})
+	}
+}
+func runRead(testFile string, b *testing.B) {
 	// Open file
 	gFile, err := os.Open(testFile)
 	if err != nil {
@@ -32,6 +52,20 @@ func runForFile(testFile string, b *testing.B) {
 
 	// Read into STL type
 	_, err = Read(gFile)
+	if err != nil {
+		b.Errorf("could not read stl: %v", err)
+	}
+}
+func runReadFile(testFile string, b *testing.B) {
+	// Open file
+	gFile, err := os.Open(testFile)
+	if err != nil {
+		b.Errorf("could not open file: %v", err)
+	}
+	defer gFile.Close()
+
+	// Read into STL type
+	_, err = ReadFile(gFile)
 	if err != nil {
 		b.Errorf("could not read stl: %v", err)
 	}
