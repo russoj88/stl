43d8c88b9b1c57f9408db0fc6fe331fd057292a0
jeff <russoj88@gmail.com>
2019-02-12 18:19:00 -0800

    Set 1 as the minimum concurrency level

--- stl/config.go
+++ stl/config.go
@@ -1,10 +1,13 @@
 package stl
 
-import "runtime"
+import (
+	"math"
+	"runtime"
+)
 
 var concurrencyLevel = uint32(runtime.NumCPU())
 
 // Allow users of stl package to override default concurrency level
 func SetConcurrencyLevel(l uint32) {
-	concurrencyLevel = l
+	concurrencyLevel = uint32(math.Max(1, float64(l)))
 }
