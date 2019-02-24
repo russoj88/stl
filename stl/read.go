c6cf8926ae6fdc63fdd49e9cc0794777918e5cfd
Jeff <russoj88@gmail.com>
2019-02-24 00:38:15 +0000

    Resolve "Use typed error values"

--- stl/read.go
+++ stl/read.go
@@ -2,23 +2,23 @@ package stl
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 )
 
-// From creates a Solid from the input
+// From creates a Solid from the input.
+// It handles both ASCII and binary formats.
 func From(r io.Reader) (Solid, error) {
 	// Use a buffered reader.  Default size is 4096 (4KB).
 	br := bufio.NewReader(r)
 
 	// Read first 5 bytes to get file type indicator.
-	indicator, err := br.Peek(5)
+	indicator, err := br.Peek(6)
 	if err != nil {
-		return Solid{}, fmt.Errorf("could not read from file: %v", err)
+		return Solid{}, ErrEmptyInput
 	}
 
 	// If indicator is "solid" then it is an ASCII file.  Otherwise binary.
-	if string(indicator) == "solid" {
+	if string(indicator) == "solid " {
 		return fromASCII(br)
 	}
 	return fromBinary(br)
