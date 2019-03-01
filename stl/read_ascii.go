b93f0dce603f99af5d111eaeebcc4571e67be218
jeff <russoj88@gmail.com>
2019-02-28 16:09:33 -0800

    remove func that now exists in split.go

--- stl/read_ascii.go
+++ stl/read_ascii.go
@@ -2,7 +2,6 @@ package stl
 
 import (
 	"bufio"
-	"bytes"
 	"strconv"
 	"strings"
 	"sync"
@@ -150,24 +149,4 @@ func extractASCIIHeader(br *bufio.Reader) (string, error) {
 	}
 
 	return strings.TrimSpace(strings.TrimPrefix(string(s), "solid")), nil
-}
-
-func splitTriangles(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	// End on input
-	if atEOF && len(data) == 0 {
-		return 0, nil, nil
-	}
-
-	// Find the data before the 7th newline
-	for n := 0; n < 7; n++ {
-		idx := bytes.IndexByte(data[advance+1:], '\n')
-		if idx < 0 {
-			// Request more data
-			return 0, nil, nil
-		}
-		advance += idx + 1
-	}
-
-	// Made it to the end of a token
-	return advance + 1, data[:advance], nil
 }
